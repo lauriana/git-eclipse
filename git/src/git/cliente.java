ccafc522d890d70c50e1ca99a34758923bb8917b
Lauriana Paludo <laurianapaludo@hotmail.com>
2016-06-23 16:21:18 -0300

    Projeto Git Inicial

---
+++ git/src/git/cliente.java
@@ -0,0 +1,20 @@
+package git;
+
+public class cliente {
+	private int Rg;
+	private String Nome;
+
+	public int getRg() {
+		return Rg;
+	}
+	public void setRg(int rg) {
+		Rg = rg;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+}
